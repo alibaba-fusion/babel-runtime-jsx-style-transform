557010aeff04882449eb0d9956d22599d5a3b376
renyou.hdd <renyou.hdd@alibaba-inc.com>
2021-03-03 10:10:20 +0800

    feat: 支持rpx2vw4style传递参数

--- src/rpx2vw4style.js
+++ src/rpx2vw4style.js
@@ -1,7 +1,7 @@
 import rpx2vw from './rpx2vw';
 import { RPX_PROPERTY_JSX } from './constants';
 
-function rpx2vw4style(input) {
+function rpx2vw4style(input, options = {}) {
   const inputType = toString.call(input);
   if (inputType === '[object Object]') {
     for (const cssProperty in input) {
@@ -9,7 +9,7 @@ function rpx2vw4style(input) {
         const value = input[cssProperty];
         if (RPX_PROPERTY_JSX.indexOf(cssProperty) !== -1) {
           const v = typeof value === 'number' ? value + 'rpx' : value
-          input[cssProperty] = rpx2vw(v);
+          input[cssProperty] = rpx2vw(v, options);
         }
       }
     }
@@ -19,7 +19,7 @@ function rpx2vw4style(input) {
       if (!style) return;
       const [key, value] = style.split(':');
       const v = typeof value === 'number' ? value + 'rpx' : value
-      return `${key.trim()}: ${rpx2vw(v.trim())}`;
+      return `${key.trim()}: ${rpx2vw(v.trim(), options)}`;
     });
     return newStyles.join(';');
   }
